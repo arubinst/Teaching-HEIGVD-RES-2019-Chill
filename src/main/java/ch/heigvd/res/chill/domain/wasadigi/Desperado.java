9e6beb512327da9523923670ac042a903364c77f
KDVL <kevin.dovale@heig-vd.ch>
2019-02-26 19:42:38 +0100

    Desperado

---
+++ src/main/java/ch/heigvd/res/chill/domain/wasadigi/Desperado.java
@@ -0,0 +1,22 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Desperado implements IProduct {
+
+    public final static String NAME = "Desperado";
+    public final static BigDecimal PRICE = new BigDecimal(3.0);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+}
---
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/DesperadoTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DesperadoTest {
+
+    @Test
+    void getName() {
+        Desperado beer = new Desperado();
+        assertEquals(beer.getName(), Desperado.NAME);
+        assertEquals(beer.getPrice(), Desperado.PRICE);
+    }
+
+    @Test
+    void getPrice() {
+
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.wasadigi.Desperado";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Desperado.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+}

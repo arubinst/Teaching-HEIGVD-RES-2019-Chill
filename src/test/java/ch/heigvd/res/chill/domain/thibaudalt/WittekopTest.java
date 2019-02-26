65b2d3930986e532189522a0f4645ecc549516b2
thibaudalt <thibaud.alt@gmail.com>
2019-02-26 18:44:19 +0100

    Add the Wittekop beer.

---
+++ src/test/java/ch/heigvd/res/chill/domain/thibaudalt/WittekopTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.thibaudalt;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WittekopTest {
+
+    @Test
+    void thePriceAndNameForDeliriumShouldBeCorrect() {
+        Wittekop beer = new Wittekop();
+        assertEquals(beer.getName(), Wittekop.NAME);
+        assertEquals(beer.getPrice(), Wittekop.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForDelirium() {
+        Bartender joe = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.thibaudalt.Wittekop";
+        OrderRequest request = new OrderRequest(2, productName);
+        OrderResponse response = joe.order(request);
+        BigDecimal expectedTotalPrice = Wittekop.PRICE.multiply(new BigDecimal(2));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}

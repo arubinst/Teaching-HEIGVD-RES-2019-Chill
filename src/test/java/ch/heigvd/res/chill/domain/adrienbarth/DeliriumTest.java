8cc1fd65ec683da421f993ca29a97365494f9a27
adrienbarth-heigvd <adrien.barth@heig-vd.ch>
2019-03-03 21:59:34 +0100

    Add Delirium beer

--- src/test/java/ch/heigvd/res/chill/domain/adrienbarth/DeliriumTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/adrienbarth/DeliriumTest.java
@@ -9,22 +9,22 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChimayTest {
+class DeliriumTest {
 
     @Test
     void thePriceAndNameForBoxerShouldBeCorrect() {
         ChimayTest beer = new ChimayTest();
-        assertEquals(beer.getName(), ChimayTest.NAME);
-        assertEquals(beer.getPrice(), ChimayTest.PRICE);
+        assertEquals(beer.getName(), DeliriumTest.NAME);
+        assertEquals(beer.getPrice(), DeliriumTest.PRICE);
     }
 
     @Test
     void aBartenderShouldAcceptAnOrderForBoxer() {
         Bartender jane = new Bartender();
-        String productName = "ch.heigvd.res.chill.domain.wasadigi.Chimay";
+        String productName = "ch.heigvd.res.chill.domain.wasadigi.Delirium";
         OrderRequest request = new OrderRequest(3, productName);
         OrderResponse response = jane.order(request);
-        BigDecimal expectedTotalPrice = ChimayTest.PRICE.multiply(new BigDecimal(3));
+        BigDecimal expectedTotalPrice = DeliriumTest.PRICE.multiply(new BigDecimal(3));
         assertEquals(expectedTotalPrice, response.getTotalPrice());
     }
 

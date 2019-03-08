50fa31a126afebf25dc56ed4912ed04be1d4e3c2
KDVL <kevin.dovale@heig-vd.ch>
2019-03-08 10:12:58 +0100

    update package name

--- src/test/java/ch/heigvd/res/chill/domain/kevindovale/DesperadoTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/kevindovale/DesperadoTest.java
@@ -1,4 +1,4 @@
-package ch.heigvd.res.chill.domain.wasadigi;
+package ch.heigvd.res.chill.domain.kevindovale;
 
 import ch.heigvd.res.chill.domain.Bartender;
 import ch.heigvd.res.chill.protocol.OrderRequest;

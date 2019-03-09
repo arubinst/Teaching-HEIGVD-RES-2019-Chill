81cd211faa955c7433b990ab096762ac427a8df3
Dwimcore <fournier.robin@gmail.com>
2019-03-09 16:02:30 +0100

    Added more beers

---
+++ src/test/java/ch/heigvd/res/chill/domain/dwimcore/GrimbergenTest.java
@@ -0,0 +1,30 @@
+package ch.heigvd.res.chill.domain.dwimcore;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrimbergenTest {
+    @Test
+    void thePriceAndNameForBoxerShouldBeCorrect() {
+        Grimbergen beer = new Grimbergen();
+        assertEquals(beer.getName(), Grimbergen.NAME);
+        assertEquals(beer.getPrice(), Grimbergen.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForBoxer() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.dwimcore.Grimbergen";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = Grimbergen.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}

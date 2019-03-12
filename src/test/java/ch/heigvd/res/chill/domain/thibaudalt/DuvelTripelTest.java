c87a89e5bf09e7b3c3485a21bc73c8ff3c2d7cff
thibaudalt <thibaud.alt@gmail.com>
2019-03-12 17:00:20 +0100

    Added DuvelTripel beer

---
+++ src/test/java/ch/heigvd/res/chill/domain/thibaudalt/DuvelTripelTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.res.chill.domain.thibaudalt;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.wasadigi.Duvel;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DuvelTripelTest {
+
+  @Test
+  void thePriceAndNameForDuvelTripelShouldBeCorrect() {
+    DuvelTripel beer = new DuvelTripel();
+    assertEquals(beer.getName(), DuvelTripel.NAME);
+    assertEquals(beer.getPrice(), DuvelTripel.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForDuvelTripel() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.thibaudalt.DuvelTripel";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = DuvelTripel.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
